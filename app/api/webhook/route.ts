1221c1fb1c21df40907f8b467a5c7cc86e131b78
tapadar13 <monsurismyname@gmail.com>
2024-02-03 12:49:58 +0530

    Update env variable name

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -6,7 +6,7 @@ import { createUser, updateUser, deleteUser } from "@/lib/actions/user.action";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
