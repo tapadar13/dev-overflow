98eab273e73b25bec75d01884d7c71e53b7a3a12
tapadar13 <monsurismyname@gmail.com>
2024-02-06 18:21:05 +0530

    Implement display of top questions for users

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,21 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import RenderTag from "./RenderTag";
-
-const hotQuestions = [
-  {
-    _id: "1",
-    title: "Explain the difference between 'Object.freeze()' and 'const'?",
-  },
-  {
-    _id: "2",
-    title: "What is React Fiber?",
-  },
-  {
-    _id: "3",
-    title: "How do you configure dynamic routes in a Next.js application?",
-  },
-];
+import { getHotQuestions } from "@/lib/actions/question.action";
 
 const popularTags = [
   {
@@ -46,6 +32,8 @@ const popularTags = [
 ];
 
 const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
+
   return (
     <section className="custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-6 overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -237,3 +237,20 @@ export async function editQuestion(params: EditQuestionParams) {
     throw error;
   }
 }
+
+export async function getHotQuestions() {
+  try {
+    await connectToDatabase();
+
+    // Find top hot questions based on views and upvotes
+    const hotQuestions = await Question.find({}).sort({
+      views: -1,
+      upvotes: -1,
+    }); // Sort by views in descending order, then upvotes in descending order
+
+    return hotQuestions;
+  } catch (error) {
+    console.error("Error fetching hot questions:", error);
+    throw error;
+  }
+}
