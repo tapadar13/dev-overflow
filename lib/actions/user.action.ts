281f8eb0c39155b4babdaec075c64f214a501720
tapadar13 <monsurismyname@gmail.com>
2024-02-07 13:19:46 +0530

    feat: Implement search functionality for the Community page

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -34,9 +34,18 @@ export async function getAllUsers(params: GetAllUsersParams) {
   try {
     await connectToDatabase();
 
-    // const { page = 1, pageSize = 20, filter, searchQuery } = params;
+    const { searchQuery } = params;
 
-    const users = await User.find({}).sort({ createdAt: -1 });
+    const query: FilterQuery<typeof User> = {};
+
+    if (searchQuery) {
+      query.$or = [
+        { name: { $regex: new RegExp(searchQuery, "i") } },
+        { username: { $regex: new RegExp(searchQuery, "i") } },
+      ];
+    }
+
+    const users = await User.find(query).sort({ createdAt: -1 });
 
     return { users };
   } catch (error) {
