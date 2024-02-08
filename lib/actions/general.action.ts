1445957614a788a6a84eeeb8239956511eb21251
tapadar13 <monsurismyname@gmail.com>
2024-02-08 15:14:08 +0530

    feat: Implement comprehensive global search feature

---
+++ lib/actions/general.action.ts
@@ -0,0 +1,157 @@
+"use server";
+
+import Tag from "@/database/tag.model";
+import User from "@/database/user.model";
+import Answer from "@/database/answer.model";
+import Question from "@/database/question.model";
+import Interaction from "@/database/interaction.model";
+
+import { RecommendedParams, SearchParams } from "./shared.types";
+import { FilterQuery } from "mongoose";
+import { connectToDatabase } from "../mongoose";
+
+const SearchableTypes = ["question", "answer", "user", "tag"];
+export async function globalSearch(params: SearchParams) {
+  try {
+    await connectToDatabase();
+
+    const { query, type } = params;
+    const regexQuery = { $regex: query, $options: "i" };
+
+    let results = [];
+
+    const modelsAndTypes = [
+      { model: Question, searchField: "title", type: "question" },
+      { model: User, searchField: "name", type: "user" },
+      { model: Answer, searchField: "content", type: "answer" },
+      { model: Tag, searchField: "name", type: "tag" },
+    ];
+
+    const typeLower = type?.toLowerCase();
+
+    if (!typeLower || !SearchableTypes.includes(typeLower)) {
+      // If no type is specified, search in all models
+      for (const { model, searchField, type } of modelsAndTypes) {
+        const queryResults = await model
+          .find({ [searchField]: regexQuery })
+          .limit(2);
+
+        results.push(
+          ...queryResults.map((item) => ({
+            title:
+              type === "answer"
+                ? `Answers containing ${query}`
+                : item[searchField],
+            type,
+            id:
+              type === "user"
+                ? item.clerkId
+                : type === "answer"
+                  ? item.question
+                  : item._id,
+          }))
+        );
+      }
+    } else {
+      // Search in the specified model type
+      const modelInfo = modelsAndTypes.find((item) => item.type === type);
+
+      if (!modelInfo) {
+        throw new Error("Invalid search type");
+      }
+
+      const queryResults = await modelInfo.model
+        .find({ [modelInfo.searchField]: regexQuery })
+        .limit(8);
+
+      results = queryResults.map((item) => ({
+        title:
+          type === "answer"
+            ? `Answers containing ${query}`
+            : item[modelInfo.searchField],
+        type,
+        id:
+          type === "user"
+            ? item.clerkId
+            : type === "answer"
+              ? item.question
+              : item._id,
+      }));
+    }
+
+    return JSON.stringify(results);
+  } catch (error: any) {
+    console.error("Error fetching search results:", error.message);
+    throw error;
+  }
+}
+
+export async function getRecommendedQuestions(params: RecommendedParams) {
+  try {
+    await connectToDB();
+
+    const { userId, page = 1, pageSize = 20, searchQuery } = params;
+
+    // find user
+    const user = await User.findOne({ clerkId: userId });
+
+    if (!user) {
+      throw new Error("user not found");
+    }
+
+    const skipAmount = (page - 1) * pageSize;
+
+    // Find the user's interactions
+    const userInteractions = await Interaction.find({ user: user._id })
+      .populate("tags")
+      .exec();
+
+    // Extract tags from user's interactions
+    const userTags = userInteractions.reduce((tags, interaction) => {
+      if (interaction.tags) {
+        tags = tags.concat(interaction.tags);
+      }
+      return tags;
+    }, []);
+
+    // Get distinct tag IDs from user's interactions
+    const distinctUserTagIds = [
+      ...new Set(userTags.map((tag: any) => tag._id)),
+    ];
+
+    const query: FilterQuery<typeof Question> = {
+      $and: [
+        { tags: { $in: distinctUserTagIds } }, // Questions with user's tags
+        { author: { $ne: user._id } }, // Exclude user's own questions
+      ],
+    };
+
+    if (searchQuery) {
+      query.$or = [
+        { title: { $regex: searchQuery, $options: "i" } },
+        { content: { $regex: searchQuery, $options: "i" } },
+      ];
+    }
+
+    const totalQuestions = await Question.countDocuments(query);
+
+    const recommendedQuestions = await Question.find(query)
+      .populate({
+        path: "tags",
+        model: Tag,
+      })
+      .populate({
+        path: "author",
+        model: User,
+      })
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    const isNext = totalQuestions > skipAmount + recommendedQuestions.length;
+
+    return { questions: recommendedQuestions, isNext };
+  } catch (error) {
+    console.error("Error getting recommended questions:", error);
+    throw error;
+  }
+}
